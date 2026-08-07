c059364103778344d5060332b8f5358d09970c1c
Sila <ducb1531@gmail.com>
2026-08-07 15:17:11 +0700

    Delete assets directory

--- assets/js/modules/pwa.js
+++
@@ -1,9 +0,0 @@
-// pwa.js
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // sw.js nằm ở thư mục gốc
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('✅ SW registered:', reg.scope))
-      .catch(err => console.warn('❌ SW registration failed:', err));
-  });
-}
